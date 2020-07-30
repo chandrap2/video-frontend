9d0d2abc8cfb50f6bf1008afc6e5dffc66147927
chandrap <chandrap31415@gmail.com>
2020-07-29 17:02:26 -0700

    font change!

--- client-test.js
+++ client-test.js
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", () => {
     let auth_window;
 
     let isTimeline = true;
-    const tabToggleStyle = "color: #638897; background-color: #ffffc9; cursor: auto;";
+    const tabToggleStyle = "color: #638897; background-color: #ffffc9; padding: 12px; border-style: solid; cursor: auto;";
 
     let timelineTab = document.getElementById("tab-timeline");
     timelineTab.addEventListener("click", () => {
